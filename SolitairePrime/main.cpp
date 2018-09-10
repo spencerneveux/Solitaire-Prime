f3a833cc7d707a4c71fe8e2155d48a257b025cdb
Spencer Neveux <spencerneveux@gmail.com>
2018-09-10 11:08:53 -0700

    Initial Commit

---
+++ SolitairePrime/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SolitairePrime
+//
+//  Created by Spencer Neveux on 9/10/18.
+//  Copyright © 2018 Spencer Neveux. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
